533beba4e6c26c39d888500a0347eb1cbd400d3a
Ezequiel Rubio <erubion@gmail.com>
2021-05-03 20:03:19 +0200

    Update texts

--- resources/texts.js
+++ resources/texts.js
@@ -8,7 +8,7 @@ module.exports = {
     endBonilista: 'Hasta aquí la bonilista, si te apetece puedes pedirme que te lea otra entrega.',
     error: 'Ups! Vaya! La hemos liado un poco y ahora no te podemos ofrecer las últimas noticias, lo estamos arreglando, pregúntame dentro de un rato a ver si ya hemos encotrado el problema.',
     errorText: 'Perdona, creo que estaba un poco despistada y no te he entendido, puedes repetirlo otra vez?',
-    helpText: 'Recuerda que puedes pedirme las bonilistas de las últimas 10 semanas, por ejemplo Leeme la última bonilista, o la bonilista de hace 2 semanas.', 
+    helpText: 'Recuerda que puedes pedirme las bonilistas de las últimas 10 semanas, por ejemplo, Leeme la última bonilista. O, leeme la bonilista de hace 2 semanas.', 
     helpTextCard: 'Recuerda que puedes pedirme las bonilistas de las últimas 10 semanas, por ejemplo Leeme la última bonilista, o la bonilista de hace 2 semanas.', 
     longPause: '<break time="2s" />',
     of: 'de',
@@ -20,6 +20,6 @@ module.exports = {
     titleThisWeek: 'Esta semana tenemos.',
     titleLastWeek: 'Hace una semana tuvimos.',
     titleNWweeks: 'Hace {n} semanas tuvimos.',
-    welcomeText: 'Te damos la bienvenida a la Bonilista, camarada del metal. Me puedes pedir que te lea las bonilistas de las últimas 10 semanas; puedes decir leeme la bonilista de hace 3 semanas o leeme la bonilista de la última semana. Si no sabes cual es la que te interesa, te puedo decir también los títulos de las bonilistas disponibles. ¿Qué prefieres?',
-    welcomeTextCard: 'Te damos la bienvenida a la Bonilista, camarada del metal. Me puedes pedir que te lea las bonilistas de las últimas 10 semanas, ¿Cúal quieres escuchar?'
+    welcomeText: 'Te damos la bienvenida a la Bonilista; ¡Camarada del metal!. Me puedes pedir que te lea las bonilistas de las últimas 10 semanas; puedes decir, leeme la bonilista de hace 3 semanas. O, leeme la bonilista de la última semana. Si no sabes cual es la que te interesa, te puedo decir también los títulos de las bonilistas disponibles. ¿Qué prefieres?',
+    welcomeTextCard: 'Te damos la bienvenida a la Bonilista; ¡Camarada del metal!. Me puedes pedir que te lea las bonilistas de las últimas 10 semanas; puedes decir, leeme la bonilista de hace 3 semanas, o leeme la bonilista de la última semana. Si no sabes cual es la que te interesa, te puedo decir también los títulos de las bonilistas disponibles. ¿Qué prefieres?'
 }
